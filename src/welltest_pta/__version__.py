93d2039cda85629fc91d95e8cd3e0728496a2226
Ismail Harkat <geoharkat@gmail.com>
2026-05-05 14:20:01 +0100

    Update version handling in __version__.py

--- src/welltest_pta/__version__.py
+++ src/welltest_pta/__version__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("welltest-pta")
+except PackageNotFoundError:
+    # Package is not installed
+    __version__ = "0.1.0"
